ae1fb131a7d868bd2cf048d37fb5b063bcdd632f
Noah Liebman <noah@noahliebman.com>
2012-06-11 22:01:53 -0500

    start of pair counts

--- counts.py
+++ counts.py
@@ -2,14 +2,25 @@ import csv
 
 data = csv.DictReader(open("thedata.txt", "rb"), delimiter="\t")
 
-counter = 0
+counter = {}
 
 for row in data:
 	sortedrow = sorted(row)
 	for i, outercol in enumerate(sortedrow):
 		j = i + 1
+		counter[outercol] = {}
 		while j < len(sortedrow):
+			# colAterms = row[sortedrow[i]].split(",")
+			# colBterms = row[sortedrow[j]].split(",")
+
+			# for colAterm in enumerate(colAterms):
+			# 	print colAterm
+
+			#print colAterms
+
+			if sortedrow[j] not in counter[outercol]: counter[outercol][sortedrow[j]] = 0
 			if row[sortedrow[i]] == row[sortedrow[j]]:
-				counter += 1
+				counter[outercol][sortedrow[j]] += 1
 			j += 1
-print counter
+		
+		print outercol + " to " + sortedrow[j] + ": " + str(counter[outercol][sortedrow[j]])
