fcdf84166ff322255b90631419b5b8e25cf1c409
Noah Liebman <noah@noahliebman.com>
2012-06-14 15:03:59 -0500

    counts look good

--- counts-coliter.py
+++ counts-coliter.py
@@ -2,7 +2,9 @@ import csv
 
 thefile = open("thedata.txt", "rb")
 data = csv.DictReader(thefile, delimiter="\t")
-for row in data: pass # need to loop through to init the data for some weird reason
+nrows = 0
+for row in data: # need to loop through to init the data for some weird reason
+	nrows += 1
 cols = data._fieldnames
 
 thefile.seek(0)
@@ -18,11 +20,23 @@ for i, col in enumerate(cols):
 		innercol = cols[j]
 		#print outercol + ", " + innercol
 		counter[outercol][innercol] = 0
+		counter[outercol]['na'] = 0
 		for row in data:
 			# print row[outercol] + " " + row[innercol]
-			if row[outercol] == row[innercol] and row[outercol] != '':
+			if row[outercol] == "NA":
+				counter[outercol]['na'] += 1
+
+
+			colAterms = row[outercol].split(",")
+			colBterms = row[innercol].split(",")
+
+			for term in colAterms:
+				if term in colBterms:
+					counter[outercol][innercol] += float(1)/max([len(colAterms), len(colBterms)])
+
+			#if row[outercol] == row[innercol] and row[outercol] != '':
 				#if outercol == "ROW No.": print row[outercol].find('\n')
-				counter[outercol][innercol] += 1
+				#counter[outercol][innercol] += 1
 		thefile.seek(0)
-		print outercol + " to " + innercol + ": " + str(counter[outercol][innercol])
+		print outercol + " to " + innercol + ": " + str(round(float(counter[outercol][innercol])/(nrows - counter[outercol]['na']) * 100))
 		j += 1
